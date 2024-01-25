d2bdfac3add731e1c47eb90f1bf2dbe32ba86c0f
mauung-sudo <maungkmthu@gmail.com>
2024-01-25 15:37:05 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function validateForm() {
+  const name = document.getElementById("frmName").value;
+  const email = document.getElementById("frmEmail").value;
+
+  function sanitizeInput(name) {
+    // Allow only alphanumeric characters for names
+    return name.replace(/[^a-zA-Z0-9]/g, "");
+  }
+
+  const frmNameError = document.getElementById("frmNameError");
+  const frmEmailError = document.getElementById("frmEmailError");
+
+  frmNameError.innerHTML = ""; // Clear previous error messages
+  frmEmailError.innerHTML = "";
+
+  if (name === "") {
+    frmNameError.innerHTML = "Name is empty";
+    document.getElementById("frmName").style.borderColor = "#e23636";
+  }
+
+  if (email === "") {
+    frmEmailError.innerHTML = "Email is empty";
+    document.getElementById("frmEmail").style.borderColor = "#e23636";
+  } else if (!validateEmail(email)) {
+    frmEmailError.innerHTML = "Invalid email format";
+    document.getElementById("frmEmail").style.borderColor = "#e23636";
+  }
+
+  if (frmNameError.innerHTML === "" && frmEmailError.innerHTML === "") {
+    // Submit the form (you might want to do an AJAX call here)
+    console.log(sanitizeInput(name));
+    document.getElementById("subscribeForm").submit();
+  }
+}
+
+function validateEmail(email) {
+  const emailRegex = /\S+@\S+\.\S+/;
+  return emailRegex.test(email);
+}
